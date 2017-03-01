7caf897534469cd435509767bf91417f2c6111e0
Hongjun Ge <ghj@Android.local>
2017-03-02 00:23:23 +0800

    modified

--- src/index.js
+++ src/index.js
@@ -28,37 +28,7 @@ class MonkeyRunner{
 	// this.exception = undefined
   }
 
-  // get runner(){
-  //   return this.runner;
-  // }
 
-  // set runner(runner){
-  //   this.runner = runner;
-  // }
-
-  // get stdout(){
-  //   return this.stdout;
-  // }
-
-  // set stdout(stdout){
-  //   this.stdout = stdout;
-  // }
-
-  // get stderr(){
-  //   return this.stderr;
-  // }
-
-  // set stderr(stderr){
-  //   this.stderr = stderr;
-  // }
-
-  // get stdin(){
-  //   return this.stdin;
-  // }
-
-  // set stdin(stdin){
-  //   this.stdin = stdin;
-  // }
 
   getRunner(){
 	  return this.runner
@@ -137,34 +107,3 @@ class MonkeyRunner{
 
 export default MonkeyRunner;
 
-
-//   this.runner.stdout.on('data',function(data){
-//     console.log(data);
-//   })
-  //
-  // runner.stderr.on('data',function(data){
-  //   console.log(data);
-  // })
-  //
-  // runner.on('close', function(code) {
-  //   console.log('closing code: ' + code);
-  // });
-
-
-
-  //
-  //  	/*将Monkey的运行结果写到文件中*/
-  //  	console.log("准备写入文件");
-  //  	fs.writeFile(`${this.path}.txt`,data,function(err){
-  // 	if(err){
-  //   		console.log("Monkey写文件Error")
-  // 	}
-  //  	})
-  //  	console.log("写入文件完成")
-  // },(err)=>{
-  //  		console.log("执行错误"+err);
-  // }).then(()=>{
-  //  		console.log("截图开始")
-  //  		setTimeout(()=>{
-  // 	exec(`adb -s ${this.deviceId} shell screencap -p | sed 's/\r$//' > ${this.screenshot}.png`);
-  //  	},5000)
